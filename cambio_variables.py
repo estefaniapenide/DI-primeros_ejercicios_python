f428a17a3f0cc6f440fb312bd9c510e990576bb6
etef <estefaniapenide@gmail.com>
2021-09-30 19:20:58 +0200

    tercer ejercicio

---
+++ cambio_variables.py
@@ -0,0 +1,16 @@
+#Dadas dos variables numéricas A y B, que el uusario debe teclear, se pide realizar un algoritmo que intercambie los valores
+#de ambas varariables y muestre cuanto vales al final las dos variables
+
+print("INTERCAMBIO DE VARIABLES\n")
+
+a=float(input("Introduzca la variable A: "))
+b=float(input("Introduzca la variable B: "))
+
+#También podría haber hecho a,b=b,a
+
+c=a
+a=b
+b=c
+
+print("\nLa variable A ahora vale %s" % (a))
+print("La variable B ahora vale %d" % (b))
