a5d650c8ba44314fe2fed95e0bc19cc4efd41e79
Estefanía Penide <estefaniapenide@gmail.com>
2021-10-01 09:26:56 +0200

    Ejercicio iniciales terminado

--- iniciales.py
+++ iniciales.py
@@ -1,5 +1,7 @@
-#Pedir el nombre y apellidos y devolver las iniciales
+#Pedir el nombre y los dos apellidos de una persona y mostar las iniciales
 
 nombre=input("Introduzca su nombre: ")
 apellido1=input("Introduzca su primer apellido: ")
 apellido2=input("Introduzca su segundo apellido: ")
+
+print("Sus iniciales son",nombre[0].upper(),".",apellido1[0].upper(),".",apellido2[0].upper())
